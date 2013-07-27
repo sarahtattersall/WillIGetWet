039075c201c6e1eb018c0506901b872f47e5dc16
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2013-07-27 10:03:08 +0100

    Add samsung fix to get current location

--- src/com/example/willigetwet/MainActivity.java
+++ src/com/example/willigetwet/MainActivity.java
@@ -1,49 +1,46 @@
 package com.example.willigetwet;
 
-import com.example.willigetwet.R;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.location.LocationClient;
-
+import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements
-		GooglePlayServicesClient.ConnectionCallbacks,
-		GooglePlayServicesClient.OnConnectionFailedListener {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 
-	private LocationClient mLocationClient;    // Global variable to hold the current location
-    private Location mCurrentLocation;
-	
+public class MainActivity extends FragmentActivity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener,
+		LocationListener {
+
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.activity_main);
+
 		mLocationClient = new LocationClient(this, this, this);
-		   
-		
-//		TextView textView = new TextView(this);
-//		textView.setTextSize(40);
-//		textView.setText("Bar");
-        setContentView(R.layout.activity_main);
 	}
-	
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-        mLocationClient.connect();
+		mLocationClient.connect();
 	}
-	
-    @Override
-    protected void onStop() {
-        // Disconnecting the client invalidates it.
-        mLocationClient.disconnect();
-        super.onStop();
-    }
+
+	@Override
+	protected void onStop() {
+		mLocationClient.disconnect();
+		super.onStop();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,22 +52,80 @@ public class MainActivity extends Activity implements
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void onConnected(Bundle arg0) {
 		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-//		mCurrentLocation = mLocationClient.getLastLocation();
-//
-    	TextView textView = (TextView) findViewById(R.id.latitude_text);
-    	textView.setText("Hello");
+		LocationManager manager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+				new LocationListener() {
+					@Override
+					public void onStatusChanged(String provider, int status,
+							Bundle extras) {
+					}
+
+					@Override
+					public void onProviderEnabled(String provider) {
+					}
+
+					@Override
+					public void onProviderDisabled(String provider) {
+					}
+
+					@Override
+					public void onLocationChanged(final Location location) {
+					}
+				});
+		mCurrentLocation = mLocationClient.getLastLocation();
+
+		if (mCurrentLocation != null)
+		{
+			TextView latTextView = (TextView) findViewById(R.id.latitude_text);
+			latTextView.setText(Double.toString(mCurrentLocation.getLatitude()));
+	
+			TextView longTextView = (TextView) findViewById(R.id.longitude_text);
+			longTextView.setText(Double.toString(mCurrentLocation.getLongitude()));
+		}
 	}
 
 	@Override
 	public void onDisconnected() {
 		Toast.makeText(this, "Disconnected. Please re-connect.",
-                Toast.LENGTH_SHORT).show();
+				Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public void onLocationChanged(final Location location) {
+	    mCurrentLocation = location;
+		if (mCurrentLocation != null) {
+			TextView latTextView = (TextView) findViewById(R.id.latitude_text);
+			latTextView
+					.setText(Double.toString(mCurrentLocation.getLatitude()));
+
+			TextView longTextView = (TextView) findViewById(R.id.longitude_text);
+			longTextView.setText(Double.toString(mCurrentLocation
+					.getLongitude()));
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
